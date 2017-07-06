1f59fd8fcc616bcee4a61f3456376708f1b31308
Andrew Torres <andrew.jonathan.torres@gmail.com>
2017-07-05 17:11:25 -0700

    :art: Restructure string utility

--- test/lib/util/string-util.test.js
+++ test/lib/util/string-util.test.js
@@ -27,6 +27,34 @@ describe('stringUtil', () => {
     });
   });
 
+  describe('.padEnd(str, length, pad)', () => {
+    it('should return the provided string', () => {
+      stringUtil.padEnd('mike', 2, '#').should.equal('mike');
+    });
+
+    it('should return a string with spaces applied to the end', () => {
+      stringUtil.padEnd('yankee', 14).should.equal('yankee        ');
+    });
+
+    it('should return a string with the provided padding applied to the end', () => {
+      stringUtil.padEnd('oscar', 10, '*').should.equal('oscar*****');
+    });
+  });
+
+  describe('.padStart(str, length, pad)', () => {
+    it('should return the provided string', () => {
+      stringUtil.padStart('romeo', 3, '@').should.equal('romeo');
+    });
+
+    it('should return a string with spaces applied to the start', () => {
+      stringUtil.padStart('whiskey', 9).should.equal('  whiskey');
+    });
+
+    it('should return a string with the provided padding applied to the start', () => {
+      stringUtil.padStart('delta', 9, '!').should.equal('!!!!delta');
+    });
+  });
+
   describe('.prepend(str, data)', () => {
     it('should return a string with data prepended', () => {
       stringUtil.prepend('bravo', 'golf hotel ').should.equal('golf hotel bravo');
