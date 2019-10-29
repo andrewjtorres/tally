2f1c0539f151e757def943a516c8894e083999cb
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-10-29 15:54:15 -0700

    refactor: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@
 const jestPlugin = require('eslint-plugin-jest')
 
 module.exports = {
+  plugins: ['import', 'prettier', 'promise', 'standard', 'unicorn'],
   extends: [
     'standard',
     'plugin:import/recommended',
@@ -12,23 +13,32 @@ module.exports = {
     'prettier/standard',
     'prettier/unicorn',
   ],
-  overrides: [
-    {
-      files: ['lib/**/__mocks__/**/*.js', 'lib/**/?(*.)test.js'],
-      globals: jestPlugin.environments.globals.globals,
-      plugins: jestPlugin.configs.recommended.plugins,
-      rules: jestPlugin.configs.recommended.rules,
-    },
-  ],
-  plugins: ['import', 'prettier', 'promise', 'standard', 'unicorn'],
   rules: {
     'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
     'no-param-reassign': ['error', { props: true }],
     'import/order': [
       'error',
-      { groups: [['builtin', 'external']], 'newlines-between': 'always' },
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index', 'unknown'],
+        ],
+        'newlines-between': 'always',
+      },
     ],
     'unicorn/prevent-abbreviations': 'off',
     'prettier/prettier': 'error',
   },
+  overrides: [
+    {
+      files: [
+        'config/jest/**/*.js',
+        'lib/**/__mocks__/**/*.js',
+        'lib/**/?(*.)test.js',
+      ],
+      globals: jestPlugin.environments.globals.globals,
+      plugins: jestPlugin.configs.recommended.plugins,
+      rules: jestPlugin.configs.recommended.rules,
+    },
+  ],
 }
