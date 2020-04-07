b861917a55ddb6fb2d50587907eb9d7cc1412adf
Andrew Torres <andrew.jonathan.torres@gmail.com>
2020-04-07 15:55:35 -0700

    chore: update yarn.lock format

--- .pnp.js
+++ .pnp.js
@@ -141,7 +141,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jest-circus", "npm:25.2.7"],
             ["lint-staged", "npm:10.1.2"],
             ["npm-run-all", "npm:4.1.5"],
-            ["prettier", "npm:2.0.3"],
+            ["prettier", "npm:2.0.4"],
             ["prettylint", "virtual:00f4dc5b5716c66b9aa8875c8c4f1330987e76fef068c4603f5f9e455c250cf6e6ff4f903024ff6dc767cbf872b43e1107107a49deabae009044d51254405fbb#npm:1.0.0"],
             ["rxjs", "npm:6.5.5"],
             ["ts-jest", "virtual:00f4dc5b5716c66b9aa8875c8c4f1330987e76fef068c4603f5f9e455c250cf6e6ff4f903024ff6dc767cbf872b43e1107107a49deabae009044d51254405fbb#npm:25.3.1"],
@@ -2521,7 +2521,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["regexpp", "npm:2.0.1"],
             ["semver", "npm:6.3.0"],
             ["strip-ansi", "npm:5.2.0"],
-            ["strip-json-comments", "npm:3.0.1"],
+            ["strip-json-comments", "npm:3.1.0"],
             ["table", "npm:5.4.6"],
             ["text-table", "npm:0.2.0"],
             ["v8-compile-cache", "npm:2.1.0"]
@@ -2710,7 +2710,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["eslint-plugin-prettier", "virtual:00f4dc5b5716c66b9aa8875c8c4f1330987e76fef068c4603f5f9e455c250cf6e6ff4f903024ff6dc767cbf872b43e1107107a49deabae009044d51254405fbb#npm:3.1.2"],
             ["eslint", "npm:6.8.0"],
-            ["prettier", "npm:2.0.3"],
+            ["prettier", "npm:2.0.4"],
             ["prettier-linter-helpers", "npm:1.0.0"]
           ],
           "packagePeers": [
@@ -2725,7 +2725,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["eslint-plugin-prettier", "virtual:6ece7cb278ca6209d4ad24fb024e604104303cda8b39e112fcf8751e7fc5e76c4ffc6997774167c4bb7ff6270907d056b2b80dac13557515e290110d81fba400#npm:2.7.0"],
             ["fast-diff", "npm:1.2.0"],
             ["jest-docblock", "npm:21.2.0"],
-            ["prettier", "npm:2.0.3"]
+            ["prettier", "npm:2.0.4"]
           ],
           "packagePeers": [
             "prettier"
@@ -6437,10 +6437,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["prettier", [
-        ["npm:2.0.3", {
-          "packageLocation": "./.yarn/cache/prettier-npm-2.0.3-9b6f69b20a-1.zip/node_modules/prettier/",
+        ["npm:2.0.4", {
+          "packageLocation": "./.yarn/cache/prettier-npm-2.0.4-8d1f5f638d-1.zip/node_modules/prettier/",
           "packageDependencies": [
-            ["prettier", "npm:2.0.3"]
+            ["prettier", "npm:2.0.4"]
           ],
           "linkType": "HARD"
         }]
@@ -6479,7 +6479,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["ignore", "npm:3.3.10"],
             ["lines-and-columns", "npm:1.1.6"],
             ["meow", "npm:3.7.0"],
-            ["prettier", "npm:2.0.3"],
+            ["prettier", "npm:2.0.4"],
             ["tslib", "npm:1.11.1"]
           ],
           "packagePeers": [
@@ -7702,6 +7702,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["strip-json-comments", "npm:3.0.1"]
           ],
           "linkType": "HARD"
+        }],
+        ["npm:3.1.0", {
+          "packageLocation": "./.yarn/cache/strip-json-comments-npm-3.1.0-fbc5f88936-1.zip/node_modules/strip-json-comments/",
+          "packageDependencies": [
+            ["strip-json-comments", "npm:3.1.0"]
+          ],
+          "linkType": "HARD"
         }]
       ]],
       ["supports-color", [
@@ -7797,7 +7804,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jest-circus", "npm:25.2.7"],
             ["lint-staged", "npm:10.1.2"],
             ["npm-run-all", "npm:4.1.5"],
-            ["prettier", "npm:2.0.3"],
+            ["prettier", "npm:2.0.4"],
             ["prettylint", "virtual:00f4dc5b5716c66b9aa8875c8c4f1330987e76fef068c4603f5f9e455c250cf6e6ff4f903024ff6dc767cbf872b43e1107107a49deabae009044d51254405fbb#npm:1.0.0"],
             ["rxjs", "npm:6.5.5"],
             ["ts-jest", "virtual:00f4dc5b5716c66b9aa8875c8c4f1330987e76fef068c4603f5f9e455c250cf6e6ff4f903024ff6dc767cbf872b43e1107107a49deabae009044d51254405fbb#npm:25.3.1"],
